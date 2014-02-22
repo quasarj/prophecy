dc3058f5d823127d2d8aece13f50272ba05984a9
Quasar Jarosz <quasar@ja.rosz.org>
2014-02-21 21:02:40 -0600

    added Window.append to append query results to current buffer

--- ftplugin/VimSQL.py
+++ ftplugin/VimSQL.py
@@ -20,6 +20,7 @@ class Window(QtGui.QWidget):
     conn = None
     cur = None
     database = None
+    data = None
 
     scroll_pause = False
 
@@ -54,6 +55,9 @@ class Window(QtGui.QWidget):
 
 
     def populate(self, data, headers):
+        self.data = data
+        self.headers = headers
+
         # clear the table
         self.table.setRowCount(0)
         self.table.setColumnCount(0)
@@ -100,6 +104,7 @@ class Window(QtGui.QWidget):
         if self.cur:
             
             data = self.cur.fetchmany(50)
+            self.data.extend(data)
 
             current = self.table.rowCount()
             self.table.setRowCount(current + len(data))
@@ -119,6 +124,10 @@ class Window(QtGui.QWidget):
         if color:
             item.setBackgroundColor(QtGui.QColor(*color))
         self.table.setItem(x, y, item)
+
+    def append(self):
+        for row in self.data:
+            vim.current.buffer.append(','.join([str(i) for i in row]))
 
     # def handleSave(self):
     #     path = QtGui.QFileDialog.getSaveFileName(
