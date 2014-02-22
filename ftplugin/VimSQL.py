9d0d8ddd776a226cc0a9aa63b5c68d3643e1f5c1
Quasar Jarosz <quasar@ja.rosz.org>
2014-02-22 09:09:15 -0600

    removed old code

--- ftplugin/VimSQL.py
+++ ftplugin/VimSQL.py
@@ -154,39 +154,6 @@ class Window(QtGui.QWidget):
         for row in self.data:
             vim.current.buffer.append(','.join([str(i) for i in row]))
 
-    # def handleSave(self):
-    #     path = QtGui.QFileDialog.getSaveFileName(
-    #             self, 'Save File', '', 'CSV(*.csv)')
-    #     if not path.isEmpty():
-    #         with open(unicode(path), 'wb') as stream:
-    #             writer = csv.writer(stream)
-    #             for row in range(self.table.rowCount()):
-    #                 rowdata = []
-    #                 for column in range(self.table.columnCount()):
-    #                     item = self.table.item(row, column)
-    #                     if item is not None:
-    #                         rowdata.append(
-    #                             unicode(item.text()).encode('utf8'))
-    #                     else:
-    #                         rowdata.append('')
-    #                 writer.writerow(rowdata)
-
-    # def handleOpen(self):
-    #     path = QtGui.QFileDialog.getOpenFileName(
-    #             self, 'Open File', '', 'CSV(*.csv)')
-    #     if not path.isEmpty():
-    #         with open(unicode(path), 'rb') as stream:
-    #             self.table.setRowCount(0)
-    #             self.table.setColumnCount(0)
-    #             for rowdata in csv.reader(stream):
-    #                 row = self.table.rowCount()
-    #                 self.table.insertRow(row)
-    #                 self.table.setColumnCount(len(rowdata))
-    #                 for column, data in enumerate(rowdata):
-    #                     item = QtGui.QTableWidgetItem(data.decode('utf8'))
-    #                     self.table.setItem(row, column, item)
-
-
 def showWindow():
     global window, app
 
